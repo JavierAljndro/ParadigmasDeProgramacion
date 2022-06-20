3715a60fc5ddec4eba6a9394b3542768277a3ebd
Javier H <javieraljndro@gmail.com>
2022-06-20 06:42:57 -0500

    Uso básico de Git

---
+++ UsoDeGit.py
@@ -0,0 +1,23 @@
+#=============================
+# USO BÁSICO DE GIT
+#=============================
+
+# Primero tenemos que entrar a la ubicacion de nuestro repositorio en nuestros archivos:
+
+cd Documentos/Paradigmas/Repositorio/ParadigmasDeProgramacion
+
+
+#Tenemos que poner el siguiente comando para crear trabajar sobre nuestro repositorio: 
+
+git clone https://github.com/JavierAljndro/ParadigmasDeProgramacion.git  #Esta URL se sacó directamente de mi repositrio de GitHub en la parte de "CODE"
+
+#----------------
+ git status #Para verificar nuestro los archivos que aún no se suben al stage 
+
+ git add "nombre del archivo"
+
+ git commit -m "descripción del archivo"
+
+ git push
+
+ #NOTA: en el pasword, hay que copiar y pegar el token que generamos 
