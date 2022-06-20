7a6ccd3947f54fe7723af1636d7f6c7427040829
Javier H <javieraljndro@gmail.com>
2022-06-20 06:39:08 -0500

    Función zip y generador

---
+++ Laboratorio 8/migenerador.py
@@ -0,0 +1,17 @@
+def migenerador():
+    print("primero")
+    yield 10
+    print("Segundo")
+    yield 20
+    print("tercero")
+    yield 30
+
+gen = migenerador()
+val1 = next(gen)
+print(val1)
+
+val2 = next(gen)
+print(val2)
+
+val3 = next(gen)
+print(val3)
